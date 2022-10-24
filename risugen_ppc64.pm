9a4afd2328c56e1b7f3601baa52b0d5d2689da57
Matheus Ferst <matheus.ferst@eldorado.org.br>
2022-10-24 14:53:55 -0300

    ppc64: use 32-bits values for each VR word

--- risugen_ppc64.pm
+++ risugen_ppc64.pm
@@ -136,7 +136,7 @@ sub write_random_ppc64_vrdata()
 {
     for (my $i = 0; $i < 32; $i++) {
         # load a random doubleword value at r0
-        write_mov_ri128(rand(0xffff), rand(0xffff), rand(0xfffff), rand(0xfffff));
+        write_mov_ri128(rand(0xffffffff), rand(0xffffffff), rand(0xffffffff), rand(0xffffffff));
         # li r0, 16
         write_mov_ri16(0, 0x10);
         # lvx vr$i, r1, r0
